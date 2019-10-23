b1bb7cff4ceec63f113698cb16e2ad00a260e638
SZDevinLi1130 <Devin.li@avnet.com>
2019-10-23 13:57:06 +0800

    this sample code base on SDK15.3

---
+++ ble_app_uart_pcm_audio/mod/pcm_data.h
@@ -0,0 +1,23 @@
+/* Copyright (c) 2015 Nordic Semiconductor. All Rights Reserved.
+ *
+ * The information contained herein is property of Nordic Semiconductor ASA.
+ * Terms and conditions of usage are described in detail in NORDIC
+ * SEMICONDUCTOR STANDARD SOFTWARE LICENSE AGREEMENT.
+ *
+ * Licensees are granted free, non-transferable use of the information. NO
+ * WARRANTY of ANY KIND is provided. This heading must NOT be removed from
+ * the file.
+ *
+ */
+#ifndef __PCM_DATA_H__
+#define __PCM_DATA_H__
+
+#include <stdint.h>
+
+// PCM test data
+extern const int16_t m_pcm_data[];
+
+// PCM test data sample number
+extern const uint32_t m_pcm_data_num;
+
+#endif
